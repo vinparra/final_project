dd048315c0843fe9b570e78dcc765915aa600dbe
Raghu Betina <raghu@firstdraft.com>
2018-03-05 22:11:34 +0000

    Generate notes resource with starter:resource

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,81 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+
+    render("notes/index.html.erb")
+  end
+
+  def show
+    @note = Note.find(params[:id])
+
+    render("notes/show.html.erb")
+  end
+
+  def new
+    @note = Note.new
+
+    render("notes/new.html.erb")
+  end
+
+  def create
+    @note = Note.new
+
+    @note.player_id = params[:player_id]
+    @note.comment = params[:comment]
+
+    save_status = @note.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/notes/new", "/create_note"
+        redirect_to("/notes")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Note created successfully.")
+      end
+    else
+      render("notes/new.html.erb")
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+
+    render("notes/edit.html.erb")
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    @note.player_id = params[:player_id]
+    @note.comment = params[:comment]
+
+    save_status = @note.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/notes/#{@note.id}/edit", "/update_note"
+        redirect_to("/notes/#{@note.id}", :notice => "Note updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Note updated successfully.")
+      end
+    else
+      render("notes/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+
+    @note.destroy
+
+    if URI(request.referer).path == "/notes/#{@note.id}"
+      redirect_to("/", :notice => "Note deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Note deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Note resource:
+  # CREATE
+  get "/notes/new", :controller => "notes", :action => "new"
+  post "/create_note", :controller => "notes", :action => "create"
+
+  # READ
+  get "/notes", :controller => "notes", :action => "index"
+  get "/notes/:id", :controller => "notes", :action => "show"
+
+  # UPDATE
+  get "/notes/:id/edit", :controller => "notes", :action => "edit"
+  post "/update_note/:id", :controller => "notes", :action => "update"
+
+  # DELETE
+  get "/delete_note/:id", :controller => "notes", :action => "destroy"
+  #------------------------------
+
   # Routes for the Eligibility resource:
   # CREATE
   get "/eligibilities/new", :controller => "eligibilities", :action => "new"
