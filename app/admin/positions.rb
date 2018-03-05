45cd743dcb565b9f461dfdc521d354c66198b78d
Raghu Betina <raghu@firstdraft.com>
2018-03-05 22:12:18 +0000

    Whitelist columns in Position for ActiveAdmin

--- app/admin/positions.rb
+++ app/admin/positions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Position do
+
+ permit_params :title
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
