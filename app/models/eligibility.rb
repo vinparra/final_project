276572a05cf70f8c1cb5d2e899fe041c74098551
Raghu Betina <raghu@firstdraft.com>
2018-03-05 22:11:42 +0000

    Direct association between positions and eligibilities

--- app/models/eligibility.rb
+++ app/models/eligibility.rb
@@ -1,6 +1,8 @@
 class Eligibility < ApplicationRecord
   # Direct associations
 
+  belongs_to :position
+
   belongs_to :player
 
   # Indirect associations
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,9 @@
 class Position < ApplicationRecord
   # Direct associations
 
+  has_many   :eligibilities,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
